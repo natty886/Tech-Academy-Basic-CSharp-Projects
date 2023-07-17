ace9d92fb740006309d43e0f2bf9b1e1376d44ed
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-17 13:34:16 -0700

    updated Part 6

--- ConsoleApp6PartAssignment/ConsoleApp6PartAssignment/Program.cs
+++ ConsoleApp6PartAssignment/ConsoleApp6PartAssignment/Program.cs
@@ -37,104 +37,104 @@ namespace ConsoleApp6PartAssignment
 
             //PART 2
             //An infinite loop.
-            Console.WriteLine("Press Enter to take a ride on the loop! (Or 'stop' to quit.)");
-            
-            
-            while (true)
-            {
-                Console.WriteLine("Woo Hoo!");
-                string input = Console.ReadLine();
-            //Fixed the infinite loop by entering 'stop'.
-                if (input == "stop")
-                {
-                    Console.WriteLine("That was fun!");
-                    break;
-                }
-                Console.ReadLine();
-
-            }
-            Console.ReadLine();
+            //            Console.WriteLine("Press Enter to take a ride on the loop! (Or 'stop' to quit.)");
 
 
-            //PART 3
-            //A loop where the comparison is determined by "<".
-            int i = 1;
-            while (i < 10)
-            {
-                Console.WriteLine(i);
-                i++;
-            }
+            //            while (true)
+            //            {
+            //                Console.WriteLine("Woo Hoo!");
+            //                string input = Console.ReadLine();
+            //            //Fixed the infinite loop by entering 'stop'.
+            //                if (input == "stop")
+            //                {
+            //                    Console.WriteLine("That was fun!");
+            //                    break;
+            //                }
+            //                Console.ReadLine();
 
-            //A loop where the comparison is determined by "<".
-            int n = 2;
-            while (n <= 10)
-            {
-                Console.WriteLine(n);
-                n++;
-            }
+            //            }
+            //            Console.ReadLine();
 
 
-            //PART 4 
-            //A list of strings where each item in the list is unique.
-            List<string> list = new List<string>();
-            list.Add("Yo");
-            list.Add("Hola");
-            list.Add("Konichiwa");
-            list.Add("Salut");
+            //            //PART 3
+            //            //A loop where the comparison is determined by "<".
+            //            int i = 1;
+            //            while (i < 10)
+            //            {
+            //                Console.WriteLine(i);
+            //                i++;
+            //            }
 
-            //Ask user to input text to search list.
-            Console.WriteLine("Enter the text you want to search for:");
-            string searchText = Console.ReadLine();
-
-            //A loop iterates through the list then displays list item index.
-            bool found = false;
-            foreach (string item in list)
-            {
-                if (item.Contains(searchText))
-                {
-                    Console.WriteLine("Found!");
-                    found = true;
-                    break;
-                }
-            }
-
-            //If text isn't on list displays message. 
-            if (!found)
-            {
-                Console.WriteLine("The text was not found.");
-            }
+            //            //A loop where the comparison is determined by "<".
+            //            int n = 2;
+            //            while (n <= 10)
+            //            {
+            //                Console.WriteLine(n);
+            //                n++;
+            //            }
 
 
-            //PART 5 
-            //A list of strings where two items are identical. Ask user to search list.
-            List<string> list = new List<string>();
-            list.Add("Booga");
-            list.Add("Booga");
-            list.Add("Oooga");
-            list.Add("Wooga");
-.
-            Console.WriteLine("Enter the text you want to search for:");
-            string searchText2 = Console.ReadLine();
+            //            //PART 4 
+            //            //A list of strings where each item in the list is unique.
+            //            List<string> list = new List<string>();
+            //            list.Add("Yo");
+            //            list.Add("Hola");
+            //            list.Add("Konichiwa");
+            //            list.Add("Salut");
 
-            //A loop iterates through the list then displays list item index. Remove break statements.
-            bool found2 = false;
-            foreach (string item in list)
-            {
-                if (item.Contains(searchText2))
-                {
-                    Console.WriteLine("Found!");
-                    found2 = true;
-                }
-            }
+            //            //Ask user to input text to search list.
+            //            Console.WriteLine("Enter the text you want to search for:");
+            //            string searchText = Console.ReadLine();
 
-            //Add code if text isn't on list displays message. 
-            if (!found2)
-            {
-                Console.WriteLine("The text was not found.");
-            }
+            //            //A loop iterates through the list then displays list item index.
+            //            bool found = false;
+            //            foreach (string item in list)
+            //            {
+            //                if (item.Contains(searchText))
+            //                {
+            //                    Console.WriteLine("Found!");
+            //                    found = true;
+            //                    break;
+            //                }
+            //            }
+
+            //            //If text isn't on list displays message. 
+            //            if (!found)
+            //            {
+            //                Console.WriteLine("The text was not found.");
+            //            }
 
 
-            //PART 5 
+            //            //PART 5 
+            //            //A list of strings where two items are identical. Ask user to search list.
+            //            List<string> list = new List<string>();
+            //            list.Add("Booga");
+            //            list.Add("Booga");
+            //            list.Add("Oooga");
+            //            list.Add("Wooga");
+            //.
+            //            Console.WriteLine("Enter the text you want to search for:");
+            //            string searchText2 = Console.ReadLine();
+
+            //            //A loop iterates through the list then displays list item index. Remove break statements.
+            //            bool found2 = false;
+            //            foreach (string item in list)
+            //            {
+            //                if (item.Contains(searchText2)
+            //                {
+            //                    Console.WriteLine(item + "Found!");
+            //                    found2 = true;
+            //                }
+            //            }
+
+            //            //Add code if text isn't on list displays message. 
+            //            if (!found2)
+            //            {
+            //                Console.WriteLine("The text was not found.");
+            //            }
+
+
+            //PART 6 
             //A list of strings where two items are identical.
             List<string> list = new List<string>();
             list.Add("Booga");
@@ -142,12 +142,26 @@ namespace ConsoleApp6PartAssignment
             list.Add("Oooga");
             list.Add("Wooga");
 
+            List<string> blankList = new List<string>();
+
             //foreach loop evaluating each item showing the string and whether or not it has already appeared in the list
             foreach (string item in list)
             {
-                bool alreadyAppeared = list.Contains(item);
-                Console.WriteLine("{0}: {1}", item, alreadyAppeared ? "Yes" : "No");
+                if (blankList.Contains(item))
+                { 
+                    Console.WriteLine(item + " has appeared already.");
+                }
+
+                else
+                {
+                    Console.WriteLine(item + " has not appeared yet.");
+                }
+
+                blankList.Add(item);
             }
+
+        }   
+            
     }
     
 }
