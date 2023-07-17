071924032b010e030b1b86b474c5060fc16cc6eb
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-17 16:29:12 -0700

    try catch

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+           
+        {
+            //try
+            //{
+            //    Console.WriteLine("Pick a number.");
+            //    int numberOne = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Pick a second number.");
+            //    int numberTwo = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Dividing the two...");
+            //    int numberThree = numberOne / numberTwo;
+            //    Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            //    Console.ReadLine();
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine("Please type a whole number.");
+            //    return;
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("Please don't divide by zero");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.ReadLine();
+            //}
+
+            // Create a list of integers
+            List<int> numbers = new List<int> { 100, 102, 103, 104, 105 };
+
+            try
+            {
+                // Ask the user for a number to divide each number in the list by
+                Console.WriteLine("Enter a number to divide each number in the list by:");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+
+                // Write a loop that takes each integer in the list divides it by the number the user entered and displays the result to the screen
+                foreach (int number in numbers)
+                {
+                    int result = number / divisor;
+                    Console.WriteLine(result);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+               
+
+
+            
+        }
+    }
+}
