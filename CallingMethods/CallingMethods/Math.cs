48b3a835cab0c5f4e047e35e1a7827ec86277cd6
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-18 12:47:29 -0700

    WIP

---
+++ CallingMethods/CallingMethods/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public static class Math
+    {
+        public static int Add(int number)
+        {
+            return number + number;
+        }
+
+        public static int Subtract(int number)
+        {
+            return number - number;
+        }
+
+        public static int Divide(int number)
+        {
+            return number / number;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        public static int Main(string[] args)
+
+        {
+            Console.WriteLine("What number would you like to do math operations on?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math();
+            Math.Add(number);
+            Math.Subtract(number);
+            Math.Divide(number);
+
+            Console.WriteLine(Math.Add(number));
+            Console.WriteLine(Math.Substract(number));
+            Console.WriteLine(Math.Divide(number));
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
