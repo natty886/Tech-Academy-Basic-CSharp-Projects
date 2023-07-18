d3c023094d60d5e10283358b551a0fc526859e0d
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-18 14:10:33 -0700

    calling methods submission

--- CallingMethods/CallingMethods/Program.cs
+++ CallingMethods/CallingMethods/Program.cs
@@ -8,19 +8,18 @@ namespace CallingMethods
 {
     class Program
     {
-        public static int Main(string[] args)
+        static void Main(string[] args)
 
         {
             Console.WriteLine("What number would you like to do math operations on?");
             int number = Convert.ToInt32(Console.ReadLine());
 
-            Math math = new Math();
             Math.Add(number);
             Math.Subtract(number);
             Math.Divide(number);
 
             Console.WriteLine(Math.Add(number));
-            Console.WriteLine(Math.Substract(number));
+            Console.WriteLine(Math.Subtract(number));
             Console.WriteLine(Math.Divide(number));
             Console.ReadLine();
 
