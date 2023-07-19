c4acf537526d40b656de79649295a741e1b9d67b
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-19 12:26:35 -0700

    WIP

---
+++ MainMethod/MainMethod/Operator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public static class Operator
+    {
+        public static int Operator(int number)
+        {
+            return number + number;
+        }
+
+        public static int Operator(decimal number)
+        {
+            int number = Decimal.ToInt32();
+            return number;
+        }
+
+        public static int Operator(string number)
+        {
+            int number = Int32.Parse(number);
+            return number;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            number = Operator.Operator(number: int, decimal, string);
+            Console.WriteLine("What number would you like to do math operations on?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(Operator.Operator(number));
+            }
+            Operator.Operator(number);
+            Console.ReadLine();
+
+
+
+           
+
+
+
+
+
+
+        }
+    }
+}
