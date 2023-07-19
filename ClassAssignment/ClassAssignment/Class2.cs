2098ce0b09372183fbd506c9c2057230fff992d8
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-19 14:49:01 -0700

    class submission

---
+++ ClassAssignment/ClassAssignment/Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Class2
+    {
+        public void DivideByTwo(int number)
+        {
+            int result = number / 2;
+            Console.WriteLine(result);
+        }
+
+        public void GetTwoNumbers(out int firstNumber, out int secondNumber)
+        {
+            firstNumber = 10;
+            secondNumber = 20;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("This is the default display method.");
+        }
+
+        public void display(int a)
+        {
+            Console.WriteLine("This is the display method with one integer parameter.");
+        }
+
+        public void display(string a, string b)
+        {
+            Console.WriteLine("This is the display method with two string parameters.");
+        }
+    }
+}
+
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 obj = new Class2();
+
+            Console.WriteLine("Enter a number");
+            int DivideByTwo = Convert.ToInt32(Console.ReadLine());
+
+            Class2 Class2 = new Class2();
+            Class2.display(10);
+
+        }
+    }
+}
