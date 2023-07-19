094ec1ddafe6a7e7e233b867e17ef6eeec18d7be
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-19 14:11:26 -0700

    void method submission

---
+++ MethodClassAssignment/MethodClassAssignment/MyMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class MyMethod
+    {
+        public void AddTwoIntegers(int firstInteger, int secondInteger)
+        {
+            int sum = firstInteger + 4;
+            int number = Convert.ToInt32(secondInteger);
+            Console.WriteLine("The sum of the two integers is {0}", number);
+        }
+    }
+}
