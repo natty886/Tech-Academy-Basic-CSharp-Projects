0c803e8baf057144fdc990ad2e761861d0066c18
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-19 13:58:54 -0700

    optional parameter assignment

---
+++ MethodAssignment/MethodAssignment/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Class1
+    {
+        public int Math(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Math(decimal number)
+        {
+            int result = Convert.ToInt32(number) + 2;
+            return result;
+        }
+
+        public int Math(string number)
+        {
+            int result = Convert.ToInt32(number);
+            return result;
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();           
+
+            Console.WriteLine("Enter a number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A second number (Not required)");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(obj.Math(number1));
+            Console.WriteLine(obj.Math(number2));
+            Console.WriteLine(obj.Math("4"));
+
+            Console.ReadLine();
+        }
+    }
+}
