6f9491106f5258db3548401c3ee80dd5e5bf30cf
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-20 10:17:27 -0700

    overloading operator

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    // 1a. Create "Employee" class 
+    public class Employee
+    {
+        // 1b. Create "Id", "FirstName" and "LastName" properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        // 2. Overload the "==" operator to check if two objects are equal by comparing Id
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+                return true;
+            else
+                return false;
+
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+                return true;
+            else
+                return false;
+        }
+
+
+
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3a. Instantiate two objects from Employee class and assign values
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            employee1.FirstName = "John";
+            employee1.LastName = "Jingleheimer";
+            employee1.Id = 72023;
+
+            employee2.FirstName = "Jacob";
+            employee2.LastName = "Smith";
+            employee2.Id = 81620;
+
+            // 3b. Compare the objects using overload operator and display results
+            Console.WriteLine(employee1.Id == employee2.Id);
+            Console.ReadLine();
+
+
+
+    }
+    }
+}
