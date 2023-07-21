54a5e92461c36fc0571f8bf9113b430bef16c98a
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-20 23:59:07 -0700

    blackjack game

---
+++ TwentyOne/TwentyOne/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public abstract class Game
+    {
+        private List<Player> _players = new List<Player>();
+        private Dictionary<Player, int> _bets = new Dictionary<Player, int>();
+
+        public List<Player> Players { get { return _players; } set { _players = value; } }
+        public string Name { get; set; }
+        public Dictionary<Player, int> Bets { get { return _bets; } set { _bets = value; } }
+        // Abstract method can only exist in abstract class
+        public abstract void Play();
+        // Method to list players: Virtual method have implementation but can be overwritten
+        public virtual void ListPlayers()
+        {
+            foreach (Player player in Players)
+            {
+                Console.WriteLine(player.Name);
+            }
+
+        }
+    }
+}
