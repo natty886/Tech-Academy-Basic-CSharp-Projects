1d1627062e3702f432e8d4b3082cbec70e41679a
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-20 17:54:03 -0700

    lambda expressions

--- LambdaAssignment/LambdaAssignment/Program.cs
+++ LambdaAssignment/LambdaAssignment/Program.cs
@@ -11,29 +11,55 @@ namespace LambdaAssignment
         static void Main(string[] args)
         {
             Employee employee = new Employee();
+
+
             // 2. Create a list of 10 employees. Two should be named “Joe”.
-            List<string> FirstName = new List<string>() { "Anna", "Bo-Banna", "Banana", "Fana", "Fo-Fanna", "Fee-Fi", "Mo", "Manna", "Joe" , "Joe" };
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee() { Id = 0, FirstName = "Joe", LastName = "Schmoe"},
+                new Employee() { Id = 1, FirstName = "Anna", LastName = "Banna" },
+                new Employee() { Id = 2, FirstName = "Bo", LastName = "Banna" },
+                new Employee() { Id = 3, FirstName = "Ban", LastName = "Anana" },
+                new Employee() { Id = 4, FirstName = "Fan", LastName = "Nah" },
+                new Employee() { Id = 5, FirstName = "Fo", LastName = "Fanna" },
+                new Employee() { Id = 6, FirstName = "Fee", LastName = "Fai" },
+                new Employee() { Id = 7, FirstName = "Mo", LastName = "Manna" },
+                new Employee() { Id = 8, FirstName = "Joey Jo-Jo Jr", LastName = "Schabadoo" },
+                new Employee() { Id = 9, FirstName = "Joe", LastName = "Dirt" }
+            };
+
 
             // 3. Use foreach loop, to create new list of "Joe"s (FYI refer property checked)
-            List<string> joes = new List<string>();
+            List<Employee> joes = new List<Employee>();
 
-            foreach (string name in FirstName)
+            foreach (Employee emp in employees)
             {
-                if (name == "Joe")
+                if (emp.FirstName == "Joe")
                 {
-                    joes.Add(name);
+                    joes.Add(emp);
                 }
             }
 
+
             // 4. Same action but with lambda expression
-            List<string> newList =  employee.FirstName.Where(n => n == "Joe").ToList();
+            List<Employee> newList =  employees.Where(n => n.FirstName == "Joe").ToList();
 
-            foreach (FirstName in newList)
+            foreach (Employee emp in newList)
             {
-                Console.WriteLine(FirstName.n);
+                Console.WriteLine(emp.FirstName);
             }
-            // 5. Use lambda to make list of all employees with Id greater than 5
 
+
+            // 5. Use lambda to make list of all employees with Id greater than 5
+            List<Employee> idsList = new List<Employee>();
+
+            foreach (Employee id in idsList)
+            {
+                if (id.Id > 5)
+                {
+                    idsList.Add(id);
+                }
+            }
             Console.ReadLine();
         }
     }
