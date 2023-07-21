f9b0567a902bebf91ea214f95ac0cee504b06bfc
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-21 10:25:53 -0700

    chaining constructors

---
+++ ConstructorAssignment/ConstructorAssignment/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class MyClass
+    {
+
+        public MyClass()
+        {
+            Console.WriteLine("Greetings!");
+        }
+
+        public MyClass(string firstName) : this()
+        {
+            Console.WriteLine("inputFirst");
+        }
+
+        public MyClass(string firstName, string lastName) : this()
+        {
+            Console.WriteLine("Nice to meet you, " + firstName + lastName);
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Create a const variable.
+            const string welcome = "Welcome!";
+            Console.WriteLine(welcome);
+
+            // 2. Create a variable using the keyword “var.”
+            var askFirst = "What is your first name?";
+            Console.WriteLine(askFirst);
+            string inputFirst = Console.ReadLine();
+
+            var askLast = "What is your last name?";
+            Console.WriteLine(askLast);
+            string inputLast = Console.ReadLine();
+
+            // 3. Chain two constructors together.
+            MyClass m1 = new MyClass(inputFirst, inputLast);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
