10c9be82489fb9e728b7a357295dedf4d1d21938
Natty Sanchez <84761201+natty886@users.noreply.github.com>
2023-07-25 19:41:56 -0700

    class first

---
+++ CodeFirstNewDatabaseSample/CodeFirstNewDatabaseSample/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstNewDatabaseSample
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
+
+
+
